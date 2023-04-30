75bba8b19461ac108eadf11194beb042e9e4dcd3
HVmech <130397692+HVmech@users.noreply.github.com>
2023-04-30 23:59:27 +0300

    Add files via upload

---
+++ move_me_out.cpp
@@ -0,0 +1,7 @@
+#include "move_me_out.h"
+
+#include <iostream>
+
+void todo_move_me_out() {
+	std::cout << "This is modified version of 'todo_move_me_out' function. It is located in the separated source file" << std::endl;
+}
